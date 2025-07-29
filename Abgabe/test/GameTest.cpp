a7a2b6e7064f45a5ed3701ef05fcc68c3fce43d1
Noah Albers <albers-noah@pm.me>
2025-07-29 12:16:23 +0200

    Refactored Makefile to properly include GTest

--- Abgabe/test/GameTest.cpp
+++ Abgabe/test/GameTest.cpp
@@ -1,8 +1,12 @@
+//#include <gtest/gtest.h>
+//#include <gmock/gmock.h>
+
 #include <gtest/gtest.h>
-#include <gmock/gmock.h>
+
+
 
 // TODO: remove this test and replace with something useful
-TEST(GameTest, example) { 
+/*TEST(GameTest, example) { 
   // Expect equality.
   EXPECT_EQ(3 * 4, 12);
-}
+}*/
