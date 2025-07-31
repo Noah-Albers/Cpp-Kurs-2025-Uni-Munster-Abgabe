5d87f588c39a357a841aa3e22aab0f8aed6ff9b9
Oliver Illenseer <oillense@uni-muenster.de>
2025-07-31 12:07:22 +0200

    AlienModelTest implementiert

--- Abgabe/test/model/AlienTest.cpp
+++ Abgabe/test/model/AlienTest.cpp
@@ -6,15 +6,15 @@
  */
  
 #include <SFML/System/Vector2.hpp>
-#include "../../src/model/Alien.h"
+#include "../../src/model/game/Alien.h"
 #include "../../src/model/Constants.hpp"
 
 #include <gtest/gtest.h>
 
 // Checks that the aliens move when they are updated
-TEST(AlienModelTest, update_moves_alien) {
+TEST(AlienModelTest, updateNormalSpeed) {
 	
-	Alien alien(0,0,2);
+	Alien alien(0,0,2, 1.0);
 	
 	auto posA = alien.getPosition();
 	
@@ -25,22 +25,49 @@ TEST(AlienModelTest, update_moves_alien) {
 	ASSERT_NE(posA,posB);	
 }
 
+TEST(AlienModelTest, updateNoSpeed) {
+	Alien alien (10, 10, 1, 0);
+	auto posA = alien.getPosition();
+
+	alien.update(0.2);
+	auto posB = alien.getPosition();
+
+	ASSERT_EQ(posA, posB);
+}
+
+TEST(AlienModelTest, updateSuperFastSpeed) {
+	Alien alien (10, 10, 1, 9876123456789.0);
+	auto posA = alien.getPosition();
+
+	alien.update(0.5);
+	auto posB = alien.getPosition();
+
+	ASSERT_NE(posA, posB);
+}
+
 TEST(AlienModelTest, setPosition_out_of_bounce_negative){
-	Alien alien(100,100,2);
+	Alien alien(100,100,2, 1.0);
 	alien.setPosition(sf::Vector2f(-10, -10));	
 	
-	ASSERT_EQ(alien.getPosition(), sf::Vector2f(0,0));
+	ASSERT_EQ(alien.getPosition(), sf::Vector2f(0,-10));
 }
 
 TEST(AlienModelTest, setPosition_out_of_bounce_positive){
-	Alien alien(100,100,2);
+	Alien alien(100,100,2, 1.0);
 	alien.setPosition(sf::Vector2f(constants::GAME_WIDTH+10, constants::GAME_HEIGHT+10));	
 	
 	ASSERT_EQ(alien.getPosition(), sf::Vector2f(constants::GAME_WIDTH,constants::GAME_HEIGHT));
 }
 
-TEST(AlienModelTest, setPosition_change_direction){
-	Alien alien(100,100,2);
+TEST(AlienModelTest, setPositionNormal){
+	Alien alien(100,100,2, 1.0);
+	alien.setPosition(sf::Vector2f(constants::GAME_WIDTH / 2, constants::GAME_HEIGHT / 2));	
+	
+	ASSERT_EQ(alien.getPosition(), sf::Vector2f(constants::GAME_WIDTH / 2,constants::GAME_HEIGHT / 2));
+}
+
+TEST(AlienModelTest, changeDirection){
+	Alien alien(100, 100, 2, 1.0);
 	auto dir = alien.getHorizontalDirection();
 	alien.changeDirection();
 	ASSERT_NE(dir, alien.getHorizontalDirection());
@@ -48,14 +75,45 @@ TEST(AlienModelTest, setPosition_change_direction){
 	ASSERT_EQ(dir, alien.getHorizontalDirection());
 }
 
-TEST(AlienModelTest, removeLife){
-	Alien alien(100,100,2);
-	ASSERT_EQ(alien.getLifes(), 2);
-	alien.removeLife();
-	ASSERT_EQ(alien.getLifes(), 1);
-	alien.removeLife();
+TEST(AlienModelTest, removeLifeShield1) {
+	Alien alien(100, 100, 0, 1.0);
+	ASSERT_FALSE(alien.hasShield());
 	ASSERT_EQ(alien.getLifes(), 0);
+
 	alien.removeLife();
+	ASSERT_FALSE(alien.hasShield());
+	ASSERT_EQ(alien.getLifes(), 0);
+}
+
+TEST(AlienModelTest, removeLifeShield2) {
+	Alien alien(100, 100, 1, 1.0);
+	ASSERT_FALSE(alien.hasShield());
+	ASSERT_EQ(alien.getLifes(), 1);
+
+	alien.removeLife();
+	ASSERT_FALSE(alien.hasShield());
+	ASSERT_EQ(alien.getLifes(), 0);
+
+	alien.removeLife();
+	ASSERT_FALSE(alien.hasShield());
+	ASSERT_EQ(alien.getLifes(), 0);
+}
+
+TEST(AlienModelTest, removeLifeShield3) {
+	Alien alien(100, 100, 3, 1.0);
+	ASSERT_TRUE(alien.hasShield());
+	ASSERT_EQ(alien.getLifes(), 3);
+
+	alien.removeLife();
+	ASSERT_TRUE(alien.hasShield());
+	ASSERT_EQ(alien.getLifes(), 2);
+
+	alien.removeLife();
+	ASSERT_FALSE(alien.hasShield());
+	ASSERT_EQ(alien.getLifes(), 1);
+
+	alien.removeLife();
+	ASSERT_FALSE(alien.hasShield());
 	ASSERT_EQ(alien.getLifes(), 0);
 }
 
