e06f2bd9fc5715f4078d07ef8a2b63a4c9bd56eb
Noah Albers <albers-noah@pm.me>
2025-08-02 14:30:59 +0200

    Finalized tests for ParticleControl

--- Abgabe/test/control/ParticleControlTest.cpp
+++ Abgabe/test/control/ParticleControlTest.cpp
@@ -1,17 +1,67 @@
+/*
+ * ParticleControlTest.cpp
+ *
+ *  Created on: 01.08.2025
+ *      Author: Alice
+ */
+ 
 #include "../../src/control/controls/ParticleControl.h"
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include <iostream>
 #include "../../src/model/Constants.hpp"
 #include "../CommonMockClasses.cpp"
-#include "../../src/assets/AssetMappings.h"
 
-TEST(ParticleControlTest, spawnParticlesAndDraw){
+#include "../../src/control/controls/AlienBulletControl.h"
+#include "../../src/control/controls/AlienControl.h"
+#include "../../src/control/controls/BulletControl.h"
+#include "../../src/control/controls/LevelControl.h"
+#include "../../src/control/controls/MeteorControl.h"
+#include "../../src/control/controls/ParticleControl.h"
+#include "../../src/control/controls/PlayerControl.h"
+#include "../../src/control/controls/UIControl.h"
 
-    sf::RenderWindow window(sf::VideoMode({10, 10}), "");
-	MockLayer layer(window);
-    ParticleControl particleControl(layer);
 
+class ParticleControlTest :  public ::testing::Test { 
+public:
+    ParticleControlTest() :
+        win(sf::VideoMode({10, 10}), "Dummy"),
+        layer(win),
+        layerbg(win),
+        alienBulletControl(layer),
+        alienControl(layer),
+        bulletControl(layer),
+        levelControl(),
+        meteorControl(layer),
+        particleControl(layer),
+        playerControl(layer),
+        uiControl(layer, layerbg)
+        {
+        alienBulletControl.populate(&playerControl);
+	    alienControl.populate(&alienBulletControl, &levelControl, &playerControl);
+	    bulletControl.populate(&alienControl, &meteorControl);
+	    meteorControl.populate(&playerControl, &particleControl);
+	    playerControl.populate(&bulletControl, &particleControl);
+	    uiControl.populate(&playerControl);
+        levelControl.populate(&alienControl, &uiControl);	
+    };
+
+    sf::RenderWindow win;
+    MockLayer layer;
+    MockLayer layerbg;
+    AlienBulletControl  alienBulletControl;
+    AlienControl        alienControl;
+    BulletControl       bulletControl;
+    LevelControl        levelControl;
+    MeteorControl       meteorControl;
+    ParticleControl     particleControl;
+    PlayerControl       playerControl;
+    UIControl           uiControl;
+};
+
+// Ensures particles are spawned from the different spawners
+// Ensures these particles are also drawn
+TEST_F(ParticleControlTest, spawns_particles_and_draws_them){
     particleControl.spawnExplosionParticle(10,10);
 
     ASSERT_EQ(particleControl.getParticles().size(),(size_t)1);
@@ -28,15 +78,10 @@ TEST(ParticleControlTest, spawnParticlesAndDraw){
         .Times(3);
 
     particleControl.draw();
-    
-
 }
 
-TEST(ParticleControlTest, update){
-    sf::RenderWindow window(sf::VideoMode({10, 10}), "");
-	MockLayer layer(window);
-    ParticleControl particleControl(layer);
-
+// Ensures particles are updated and dead ones are removed
+TEST_F(ParticleControlTest, update){
     particleControl.spawnExplosionParticle(10,10);
 
     particleControl.update(constants::PARTICLE_EXPLOSION_LENGTH/2);
@@ -46,7 +91,4 @@ TEST(ParticleControlTest, update){
     particleControl.update(constants::PARTICLE_EXPLOSION_LENGTH+1);
 
     ASSERT_TRUE(particleControl.getParticles().empty());
-
-
-
 }
